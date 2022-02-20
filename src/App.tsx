79178b335982ad042e3a243baf02569a6e579da3
Damian Ziobro <devdamianziobro@gmail.com>
2022-02-20 21:30:37 +0100

    add react router and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import {
+    Route,
+    Routes
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<div>home</div>}/>
+                    <Route path="/:imageId" element={<div>id</div>}/>
+                    <Route path="*" element={<div>not found</div>}/>
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;
